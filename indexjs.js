8a7613b074ad73bb4a184b593b7892bcc381c57b
DigitalDrago <snipsmurrcat@gmail.com>
2018-09-13 16:49:17 -0500

    Update indexjs.js

--- indexjs.js
+++ indexjs.js
@@ -5,6 +5,10 @@
 </head>
 <body>
 
+<script>NekoType="blue"</script>
+<h1 id=nl><script src="https://webneko.net/n20171213.js"></script><a 
+href="https://webneko.net">Neko</a></h1>
+  
 <script type="text/javascript">
   alert("Press OK to OK");
   </script>
